ec17fd4ec590b49744a7c0d19cd3a503e92c4c9d
Prathiksha <sprathikshaa21@gmail.com>
2025-08-01 08:50:35 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+	String name;
+	int id;
+	String department;
+	public Student(int id,String name,String department) {
+		this.id=id;
+		this.name=name;
+		this.department=department;
+	}
+	
+
+}
