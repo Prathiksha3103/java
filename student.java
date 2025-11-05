4ae0c5c8df896cbcea77217161893d5b73550334
Prathiksha <sprathikshaa21@gmail.com>
2025-11-05 19:25:57 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Student(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
